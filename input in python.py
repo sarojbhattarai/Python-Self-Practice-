944d33187f78eca1ce8d59caf2bc9e2f1eb465af
Saroj <sarojbhattarai800@gmail.com>
2018-07-08 09:15:34 +0545

    Taking Input in Python

---
+++ input in python.py
@@ -0,0 +1,18 @@
+''' input in python'''
+
+'''It takes string as input'''
+a = input("enter a number ")
+c = a+a
+print(c)
+
+
+'''for int input we can write as'''
+a = int(input("Enter a number"))
+b = a+a
+print(b)
+
+
+location = input("Your location?")
+print("You are living in "+location)
+
+
