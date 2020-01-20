8ba7c2e70739bf3ea9984364cc2e6d484893c7b9
sarojbhattarai <sarojbhattarai800@gmail.com>
2020-01-20 19:38:41 +0545

    dictionares creatin types

---
+++ dictionaries.py
@@ -0,0 +1,6 @@
+dict1 = dict({"id": 161732,"name":"Saroj",  "college":"Nepal College of Information Technonoly"})
+dict2 = dict(id = 161732, name = "Saroj", college = "Nepal College of Information Technology")
+dict3 = dict(zip(("id","name", "rollno"),(161732, "Saroj", "Nepal College of Information Techonlogy")))
+dict4 = {"id":161732, "name":"Saroj", "college":"Nepal College of Information Technology"}
+
+print(dict1,dict2,dict3,dict4)
